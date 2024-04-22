381473da9a4900b362b497ad8cec41e5d679033b
arcorin <corinacara@yahoo.com>
2024-04-22 15:32:51 +0300

    create components, introduce props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
+import TaskCard from './components/task-card/TaskCard';
 
 function App() {
+ 
+  const data = [
+    {
+      id: "T-1",
+      name: "Create a Design System for Enum Workspace.",
+      status: "Todo",
+      dueDate: new Date(2023, 5, 21)
+    },
+    {
+      id: "T-2",
+      name: "Create a Design System for Enum Workspace.",
+      status: "In Progress",
+      dueDate: new Date(2023, 7, 24)
+    },
+    {
+      id: "T-3",
+      name: "Create a Design System for Enum Workspace.",
+      status: "Complete",
+      dueDate: new Date(2023, 6, 11)
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-        <p>Start Learning React!</p>
-      </header>
+    <div className="app-container">
+      <TaskCard 
+        id={data[0].id}
+        status={data[0].status}
+        name={data[0].name}
+        dueDate={data[0].dueDate}
+      />
+      <TaskCard 
+        id={data[1].id}
+        status={data[1].status}
+        name={data[1].name}
+        dueDate={data[1].dueDate}
+      />
+      <TaskCard 
+        id={data[2].id}
+        status={data[2].status}
+        name={data[2].name}
+        dueDate={data[2].dueDate}
+      />
+      
     </div>
   );
 }
