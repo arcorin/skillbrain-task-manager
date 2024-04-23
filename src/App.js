77f2419a55c867f1381c406098fc9bef8e7b5b28
arcorin <corinacara@yahoo.com>
2024-04-23 19:05:52 +0300

    add 2 components to TaskCard: Badge & DateContainer

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
   const data = [
     {
       id: "T-1",
-      name: "Create a Design System for Enum Workspace.",
+      name: "11 Create a Design System for Enum Workspace.",
       status: "Todo",
-      dueDate: new Date(2023, 5, 21)
+      dueDate: new Date(2023, 4, 21)
     },
     {
       id: "T-2",
-      name: "Create a Design System for Enum Workspace.",
+      name: "12 Create a Design System for Enum Workspace.",
       status: "In Progress",
       dueDate: new Date(2023, 7, 24)
     },
     {
       id: "T-3",
-      name: "Create a Design System for Enum Workspace.",
+      name: "13 Create a Design System for Enum Workspace.",
       status: "Complete",
       dueDate: new Date(2023, 6, 11)
     }
--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,4 +1,6 @@
 import './TaskCard.css';
+import Badge from '../badge/Badge.js';
+import DateContainer from '../date-container/DateContainer.js';
 
 function TaskCard(props) {
     
@@ -6,18 +8,13 @@ function TaskCard(props) {
     <div className="card-wrapper">
         <div className="card-header">
             <p className="task-id">{props.id}</p>
-            <div className="badge">
-                <p>{props.status}</p>
-            </div>
+            <Badge status={props.status} />
         </div>
         <div className="card-content">
             <p>{props.name}</p>
         </div>
         <div className="card-footer">
-            <div className='due-date'>
-                <p>Due Date</p>
-                <p>{props.dueDate.toLocaleDateString()}</p>
-            </div>
+            <DateContainer date={props.dueDate} />
         </div>
     </div>
     );
