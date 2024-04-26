61d494d497c49ffbced1516595d3e9ea1b8e8518
arcorin <corinacara@yahoo.com>
2024-04-26 17:43:15 +0300

    state - introduction

--- src/components/task-card/TaskCard.jsx
+++ src/components/task-card/TaskCard.jsx
@@ -1,21 +1,25 @@
-import React from "react";
-import './TaskCard.css';
+import React, { useState } from "react";
+import './TaskCard.css'; 
 import Badge from '../badge/Badge';
 import DateContainer from '../date-container/DateContainer';
 
 function TaskCard(props) {
-    let taskId = props.id;
+    let [taskId, setTaskId] = useState(props.id); 
+    // const state = useState(props.id);
+    // const taskId = state[0];
+    // const setTaskId = state[1];  
+
+    // let taskId = props.id;
 
     const handleClick = () => {
-        console.log("You just clicked on Card no." + taskId + ".");
-        taskId += "Clicked!";
-
+         setTaskId("Clicked!");
+         console.log(taskId);
     };
     
     return (
     <div className="card-wrapper">    
         <div className="card-header">
-            <p className="task-id">{props.id}</p>
+            <p className="task-id">{taskId}</p>
             <Badge status={props.status} />
         </div>
         <div className="card-content">
